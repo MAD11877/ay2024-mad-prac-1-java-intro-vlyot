15d465498225f9cdcb30296e5693b2af645bace3
vlyot <144824996+vlyot@users.noreply.github.com>
2024-04-15 15:14:18 +0800

    h

--- Question5.java
+++ Question5.java
@@ -27,6 +27,33 @@ public class Question5
      */
      
     Scanner in = new Scanner(System.in);
+    int count = in.nextInt();
+    int[] numbers = new int[count];
+    for (int i = 0; i < count; i++) {
+    numbers[i] = in.nextInt();
+    }
+    int mode = -1; 
+    int maxCount = 0;
+    for (int i = 0; i < count; i++) {
+        int currentNumber = numbers[i];
+        int currentCount = 0;
+
+
+        for (int j = 0; j < count; j++) {
+            if (numbers[j] == currentNumber) {
+                currentCount++;
+            }
+        }
+
+        if (currentCount > maxCount) {
+            mode = currentNumber;
+            maxCount = currentCount;
+        }
+    }
+
+    System.out.println(mode);
+
+    in.close();
     
   }
 }

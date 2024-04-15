94a659e26f3c2f85b70b076ba05273d27d8575f1
vlyot <144824996+vlyot@users.noreply.github.com>
2024-04-15 14:47:49 +0800

    GGG

--- Question2.java
+++ Question2.java
@@ -18,7 +18,8 @@ public class Question2
     double weight = in.nextDouble();
 
     double bmi = weight / (height * height);
-    System.out.println("Your BMI is: " + bmi );
+    System.out.println("Your BMI is:  " + bmi );
+    System.out.println(bmi);
     in.close();
   }
 }

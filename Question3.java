801781fbba4db6b7ffed3b5b68e495de3d2fa0d5
vlyot <144824996+vlyot@users.noreply.github.com>
2024-04-15 14:38:58 +0800

    2222

--- Question3.java
+++ Question3.java
@@ -11,6 +11,11 @@ public class Question3
      */
      
     Scanner in = new Scanner(System.in);
+    System.out.println("Enter an integer: ");
+    int inte = in.nextDouble();
+
+    system.out.println(inte*inte);
+    in.close()
     
   }
 }
